54df8cbafca97a14f4237c3d1a351ee61d4f1785
Ilya Tretyakov <it@it3xl.com>
2014-05-04 16:55:58 +0400

    Job is done.

---
+++ Weak Event Solution/WeakEventProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+namespace WeakEventProject
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var myObject = new MyClassWithWeakEvent();
+
+			myObject.MyWeakEvent += MyWeakEventHandler;
+			myObject
+				.OnMyWeakEvent(new Object(), new EventArgs());
+
+			myObject.MyWeakEvent -= MyWeakEventHandler;
+			MessageBox.Show("No more events. Goodbye.");
+			myObject
+				.OnMyWeakEvent(new Object(), new EventArgs());
+		}
+
+		private static void MyWeakEventHandler(Object sender, EventArgs args)
+		{
+			MessageBox.Show("The Weak Event pattern works.");
+		}
+
+	}
+}
---
+++ Weak Event Solution/WeakEventTestProject/WeakEventBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WeakEventProject;
+
+namespace WeakEventTestProject
+{
+	[TestClass]
+	public class WeakEventBehaviour
+	{
+		private static Int32 _staticValue;
+
+		[TestMethod]
+		public void WeakEventWorks()
+		{
+			_staticValue = 0;
+			var myObject = new MyClassWithWeakEvent();
+
+			myObject.MyWeakEvent += MyWeakEventHandler;
+			myObject.OnMyWeakEvent(new Object(), new EventArgs());
+
+			myObject.MyWeakEvent -= MyWeakEventHandler;
+			myObject.OnMyWeakEvent(new Object(), new EventArgs());
+
+			Assert.AreEqual(1, _staticValue);
+		}
+
+		private static void MyWeakEventHandler(Object sender, EventArgs args)
+		{
+			_staticValue++;
+		}
+
+	}
+}
